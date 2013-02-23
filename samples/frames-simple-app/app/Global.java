f223ea6eae0e6a26ba2767deb72dfaf22683b116
Shigeru GOUGI <sgougi@gmail.com>
2013-02-23 22:18:41 +0900

    updated

--- samples/frames-simple-app/app/Global.java
+++ samples/frames-simple-app/app/Global.java
@@ -7,14 +7,17 @@ import com.wingnest.play2.frames.GraphDB;
 
 public class Global extends GlobalSettings {
 
+	private static boolean init = false;
+	
 	public void onStart(Application app) {
-
 		try {
-			GraphDB.createKeyIndex("className", Vertex.class);
+			if(!init) {
+				GraphDB.createKeyIndex("className", Vertex.class);
+				init = true;
+			}
 		} catch (java.lang.UnsupportedOperationException e){
 			Logger.info(e.getMessage());
 		}
-
 	}
 
 }
