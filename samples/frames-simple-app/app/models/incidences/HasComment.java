8e4d78fe1bce81438683112021785eefb0575011
Shigeru GOUGI <sgougi@gmail.com>
2014-02-15 19:05:28 +0900

    modified : @Domain->@OutVertex, @Range -> @InVertex

--- samples/frames-simple-app/app/models/incidences/HasComment.java
+++ samples/frames-simple-app/app/models/incidences/HasComment.java
@@ -5,16 +5,16 @@ import java.util.Date;
 import models.vertices.Comment;
 import models.vertices.Log;
 
-import com.tinkerpop.frames.Domain;
-import com.tinkerpop.frames.Range;
+import com.tinkerpop.frames.OutVertex;
+import com.tinkerpop.frames.InVertex;
 import com.wingnest.play2.frames.annotations.DateProperty;
 
 public interface HasComment extends EdgeBase {
 
-	@Domain
+	@OutVertex
 	public Log getDomain();
 
-	@Range
+	@InVertex
 	public Comment getComment();
 
 	@DateProperty("createdDate")
